c0d61686e47aab2cab4988896fd56b243aa193fc
khalilbellil <khalilbellil.ca@gmail.com>
2020-11-17 16:12:36 -0500

    auth test

--- styles/next.config.js
+++ styles/next.config.js
@@ -2,6 +2,8 @@ module.exports = {
     env: {
         NEXTAUTH_URL = 'https://next-rf.vercel.app',
         GITHUB_ID = '788159c7f3be7a90b75a',
-        GITHUB_SECRET = '50c732ca6442262538c543ccfa0c67ef4ab5865e'
+        GITHUB_SECRET = '50c732ca6442262538c543ccfa0c67ef4ab5865e',
+        FACEBOOK_CLIENT_ID = '372060077386839',
+        FACEBOOK_CLIENT_SECRET = 'a6fc6bdc0da8ad6f1a71b3ec58ba2cb1'
     },
 }
