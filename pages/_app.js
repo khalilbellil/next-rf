f4dbd8b70ab6ba0bd085fee8558e6ac5feab9c69
khalilbellil <khalilbellil.ca@gmail.com>
2020-11-17 15:03:36 -0500

    authentification test

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,14 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "../styles/main.css"
 import Layout from '../layouts/Layout'
+import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
-    return <Layout><Component {...pageProps} /></Layout>
+    return <Provider session={pageProps.session}>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </Provider>
 }
 
 export default MyApp
