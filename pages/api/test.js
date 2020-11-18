5742b58c5aa8ef6aa438f7a5839621030eb18cb9
khalilbellil <khalilbellil.ca@gmail.com>
2020-11-17 23:53:12 -0500

    db connection done

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,7 +1,7 @@
 const db = require('../../lib/db')
 const escape = require('sql-template-strings')
 
-export default function getTest(req, res){
+export default async function getTest (req, res){
     const users = await db.query(escape`
         SELECT *
         FROM user
