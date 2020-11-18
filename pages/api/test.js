b0ad7a422ad8a5b60f69162cbd5803058ebddf3b
khalilbellil <khalilbellil.ca@gmail.com>
2020-11-18 11:52:00 -0500

    db works in local, needs to be tested in deployement

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,14 +1,14 @@
 const db = require('../../lib/db')
 const escape = require('sql-template-strings')
 
-export default async function getTest (req, res){
+module.exports = async (req, res) => {
     const users = await db.query(escape`
         SELECT *
         FROM user
         ORDER BY id
         LIMIT 10
     `)
-    res.json({
+    res.status(200).json({
         test: "allo",
         users: users
     })
--- styles/next.config.js
+++ styles/next.config.js
@@ -4,6 +4,10 @@ module.exports = {
         GITHUB_ID = '788159c7f3be7a90b75a',
         GITHUB_SECRET = '50c732ca6442262538c543ccfa0c67ef4ab5865e',
         FACEBOOK_CLIENT_ID = '372060077386839',
-        FACEBOOK_CLIENT_SECRET = 'a6fc6bdc0da8ad6f1a71b3ec58ba2cb1'
+        FACEBOOK_CLIENT_SECRET = 'a6fc6bdc0da8ad6f1a71b3ec58ba2cb1',
+        MYSQL_HOST = '23.251.131.103', 
+        MYSQL_USER = 'khalilb', 
+        MYSQL_DATABASE = 'renofaciledz', 
+        MYSQL_PASSWORD = 'kookie09'
     },
 }
