89fc106c2fcbcdb437dd8b4a7f6400d312d1e0c6
khalil bellil <khalilbellil.ca@gmail.com>
2021-02-05 19:37:53 -0500

    a

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ module.exports = {
         GITHUB_SECRET : '50c732ca6442262538c543ccfa0c67ef4ab5865e',
         FACEBOOK_CLIENT_ID : '372060077386839',
         FACEBOOK_CLIENT_SECRET : 'a6fc6bdc0da8ad6f1a71b3ec58ba2cb1',
-        SQL_RENOFACILE_DZ_IP : 'localhost', 
+        SQL_RENOFACILE_DZ_IP : '34.65.240.105', 
         SQL_RENOFACILE_DZ_USERNAME : 'root', 
-        MYSQL_DB : 'renofacile', 
+        MYSQL_DB : 'renofacile_ma', 
         SQL_RENOFACILE_DZ_PASSWORD : 'Kookie&09'
         //SQL_RENOFACILE_DZ_PASSWORD : 'abc123...'
     },
