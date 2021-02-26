55ff9f7858a85b6d2ff156b820c1f212b95e280d
khalil bellil <khalilbellil.ca@gmail.com>
2021-02-25 22:10:58 -0500

    fix

--- pages/intranet/contractor-lead-manager.js
+++ pages/intranet/contractor-lead-manager.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import 'reactjs-popup/dist/index.css'
-import ClmAddLead from '../../components/intranet/ClmAddLead'
+// import ClmAddLead from '../../components/intranet/ClmAddLead'
 
 export default function contractorLeadManager() {
 
     return (
         <div>
-            <ClmAddLead />
+            {/* <ClmAddLead /> */}
         </div>
     )
 }
--- pages/intranet/tableau-de-bord.js
+++ pages/intranet/tableau-de-bord.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Dashboard from '../../components/Dashboard'
+// import Dashboard from '../../components/Dashboard'
 
 export default function tableauDeBord() {
     return (
         <div>
             <h3>Tableau de bord intranet</h3>
-            <Dashboard/>
+            {/* <Dashboard/> */}
         </div>
     )
 }
