e5b1d906c7867e45f884ebe6ed8ab4b19785fa72
khalil bellil <khalilbellil.ca@gmail.com>
2021-03-09 11:57:51 -0500

    a

--- pages/extranet/tableau-de-bord.js
+++ pages/extranet/tableau-de-bord.js
@@ -1,15 +1,7 @@
 import React from 'react'
 
 export default function tableauDeBord() {
-    const [ session, loading ] = useSession()
     return <>
-        {!session && <>
-            Not signed in <br/>
-            <button onClick={signIn}>Sign in</button>
-        </>}
-        {session && <>
-            Hello {session.user.name}, your are signed in as {session.user.email} <br/>
-            <button onClick={signOut}>Sign out</button>
-        </>}
+    test
     </>
 }
