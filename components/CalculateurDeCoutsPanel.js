4ddcdb3726e70471b8f6e2124d747021e7eaa38a
khalil bellil <khalilbellil.ca@gmail.com>
2021-04-08 16:49:04 -0400

    calculateur-panel

--- components/CalculateurDeCoutsPanel.js
+++ components/CalculateurDeCoutsPanel.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import { Button, FormGroup, Input, Label, Table } from 'reactstrap'
+import React, { useState, useEffect, useRef } from 'react'
+import { Button, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader, Table } from 'reactstrap'
+import Tree from 'react-d3-tree';
 
 export default function CalculateurDeCoutsPanel() {
     const [services, setServices] = useState(undefined)
     const [actualService, setActualService] = useState(undefined)
-
     const [questions, setQuestions] = useState(undefined)
+    const [selectedNode, setSelectedNode] = useState({})
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
     
     useEffect(() => {
         resetUI()
@@ -124,9 +127,109 @@ export default function CalculateurDeCoutsPanel() {
             }
             </>)
     }
+    const generateAnswersThenQuestionsHTML2 = (item) => {
+        return (
+            <ul>
+            <li>
+                <Input type="checkbox" name="tall-1" id="tall-1" />
+                <label for="tall">
+                <div class="ball" data-id="1"></div>
+                <Input type="text" className="col-1" value={item.id} disabled/>
+                <Input type="text" className="col-8" value={item.id_question} disabled/>
+                <Input type="text" value={item.id_next_question} disabled/>
+                <Input type="text" value={item.name} disabled/>
+                <Input type="text" className="col-8" value={item.min_price} disabled/>
+                <Input type="text" value={item.influence_method} disabled/>
+                <Input type="text" value={item.influence_value} disabled/>
+                <p><i className="far fa-edit mr-3" style={{cursor: "pointer"}}></i><i className="far fa-folder-open" style={{cursor: "pointer"}}></i></p>
+                <div class="action-list">
+                    <i class="fa fa-trash"></i>
+                    <i class="fa fa-plus"></i>
+                </div>
+                </label>
+                {
+                    (item?.question)?(<>
+                        <ul class="treeview checklist">
+                        <li>
+                            <Input type="checkbox" name="tall" id="tall-2" />
+                            <label for="tall">
+                            <div class="ball" data-id="1"></div>
+                            <Input type="text" className="col-1" value={item?.question.id} disabled/>
+                            <Input type="text" className="col-8" value={item?.question.name} disabled/>
+                            <Input type="text" value={item?.question.answer_is_a_field} disabled/>
+                            <Input type="text" value={item?.question.id_field_next_question} disabled/>
+                            <p><i className="far fa-edit mr-3" style={{cursor: "pointer"}}></i><i className="far fa-folder-open" style={{cursor: "pointer"}}></i></p>
+                            <div class="action-list">
+                                <i class="fa fa-trash"></i>
+                                <i class="fa fa-plus"></i>
+                            </div>
+                            </label>
+
+                            {
+                                (item?.answers?.length > 0)?item.answers.map((item2, y) => {
+                                    return generateAnswersThenQuestionsHTML2(item2) 
+                                }):''
+                            }
+
+                        </li>
+                        </ul>
+
+                    </>):''
+                }
+
+            </li>
+            </ul>
+            )
+    }
+    const generateServiceContentHTML = (questions) => {
+        return(<>
+            <div className="row pl-3 pr-3 pt-2">
+                <div className="col p-0 ml-2">
+
+
+                    <ul class="treeview checklist">
+                    <li>
+                        <Input type="checkbox" name="tall" id="tall" />
+                        <label for="tall">
+                        <div class="ball" data-id="1"></div>
+                        <Input type="text" className="col-1" value={questions.question.id} disabled/>
+                        <Input type="text" className="col-8" value={questions.question.name} disabled/>
+                        <Input type="text" value={questions.question.answer_is_a_field} disabled/>
+                        <Input type="text" value={questions.question.id_field_next_question} disabled/>
+                        <p><i className="far fa-edit mr-3" style={{cursor: "pointer"}}></i><i className="far fa-folder-open" style={{cursor: "pointer"}}></i></p>
+                        <div class="action-list">
+                            <i class="fa fa-trash"></i>
+                            <i class="fa fa-plus"></i>
+                        </div>
+                        </label>
+
+                        {
+                            (questions?.answers?.length > 0)?(questions.answers.map((item, i) => {
+                                return generateAnswersThenQuestionsHTML2(item)
+                            })):''
+                        }
+
+                    </li>
+                    </ul>
+
+                </div>
+            </div>
+            
+            </>)
+    }
+
+    const onNodeClick = (e) => {
+        setSelectedNode(e.attributes)
+        toggle()
+    }
+    const handleSaveClick = (e) => {
+        
+    }
 
     return (
         <div className="col" style={{border: "3px solid #00517E", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"}}>
+            <section id="visualisation">
+        </section>
             <div className="row pl-3 pr-3">
                 <FormGroup className="col">
                     <Label for="services">Choisir un service</Label>
@@ -172,40 +275,31 @@ export default function CalculateurDeCoutsPanel() {
                         </div>
                     </div>
                     {
-                        (questions)?(<>
-                        <div className="row pl-3 pr-3 pt-2">
-                            <div className="col p-0 ml-2" style={{border: "2px solid red", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)", borderRadius:"6px"}}>
-                                <Table>
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>name</th>
-                                            <th>answer_is_a_field</th>
-                                            <th>id_field_next_question</th>
-                                            <th>Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">{questions.question.id}</th>
-                                            <td>{questions.question.name}</td>
-                                            <td>{questions.question.answer_is_a_field}</td>
-                                            <td>{questions.question.id_field_next_question}</td>
-                                            <td><i className="far fa-edit mr-3" style={{cursor: "pointer"}}></i><i className="far fa-folder-open" style={{cursor: "pointer"}}></i></td>
-                                        </tr>
-                                    </tbody>
-                                </Table>
-                            </div>
-                        </div>
-                        {
-                            (questions?.answers?.length > 0)?(questions.answers.map((item, i) => {
-                                return generateAnswersThenQuestionsHTML(item)
-                            })):''
-                        }
-                        </>):''
+                        (questions)?(<div className="row pl-3 pr-3 pt-2"><div className="col p-0" style={{border: "2px solid #00517E", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)", borderRadius:"6px", minHeight:"800px"}}>
+                            <Tree data={questions} pathFunc="diagonal" orientation="vertical" onNodeClick={(e) => onNodeClick(e)} collapsible={false}/>
+                        </div></div>):''
                     }
                     </>):''
                 }
+
+                <Modal isOpen={modal} toggle={toggle}>
+                    <ModalHeader toggle={toggle}>Modification</ModalHeader>
+                    <ModalBody>
+                        {
+                            (selectedNode)?Object.keys(selectedNode).map((item, i) => {
+                                return (<>
+                                    <Label>{item}</Label>
+                                    <Input type="text" value={selectedNode[item]} disabled={(item === 'id')}/>
+                                </>)
+                            }):''
+                        }
+                    </ModalBody>
+                    <ModalFooter>
+                    <Button color="primary" onClick={handleSaveClick}>Sauvegarder</Button>{' '}
+                    <Button color="secondary" onClick={toggle}>Annuler</Button>
+                    </ModalFooter>
+                </Modal>
+
             <br/>
         </div>
     )
--- rf_toolbox/global.js
+++ rf_toolbox/global.js
@@ -9,26 +9,70 @@ export async function getDepartmentId(code){
 export async function getNextQuestionAndAnswers(id_next_question){
     let three = {}
     const first = await db.query(`SELECT * FROM sr_cost_calculator_question WHERE id='${id_next_question}'`)
-    three.question = first[0]
-    three.answers = await db.query(`SELECT * FROM sr_cost_calculator_answer WHERE id_question='${id_next_question}' ORDER BY id ASC LIMIT 50`)
-    three.answers = await getNewAnswers(three.answers)
+    three.name = first[0].name
+    three.attributes = first[0]
+
+    three.children = await getChildren(id_next_question, true)
     return three
 }
 
-export async function getNewAnswers(tree_answers){
-    for (let i = 0; i < tree_answers.length; i++) {
-        if(tree_answers[i].id_next_question && tree_answers[i].id_next_question != 0){
-            tree_answers[i].question = await db.query(`SELECT * FROM sr_cost_calculator_question WHERE id='${tree_answers[i].id_next_question}'`)
-            tree_answers[i].question = tree_answers[i].question[0]
-            
-            if(tree_answers[i].question.answer_is_a_field == 1){
-                tree_answers[i].answers = [{answer_is_a_field: 1, id_next_question: tree_answers[i].question.id_field_next_question}]
-            }else{
-                tree_answers[i].answers = await db.query(`SELECT * FROM sr_cost_calculator_answer WHERE id_question='${tree_answers[i].id_next_question}' ORDER BY id ASC LIMIT 50`)
-            }
-            tree_answers[i].answers = await getNewAnswers(tree_answers[i].answers)
-        }
+export async function getChildren(id_next_question, isAnAnswer, isField = false){
+    let new_children = []
+    let answers = undefined
+
+    if(isAnAnswer){
+        answers = await db.query(`SELECT * FROM sr_cost_calculator_answer WHERE id_question='${id_next_question}' ORDER BY id ASC LIMIT 50`)
+    }else{
+        answers = await db.query(`SELECT * FROM sr_cost_calculator_question WHERE id='${id_next_question}'`)
     }
-    return tree_answers
+    if(answers.length > 0){
+        if(answers[0]?.answer_is_a_field == 1){
+            new_children[0] = {
+                name: answers[0].name,
+                attributes: answers[0],
+                children: [
+                    {
+                        name:'FIELD',
+                        attributes:{
+                            answer_is_a_field: 1,
+                            id_next_question: answers[0].id_field_next_question
+                        },
+                        children: await getChildren(answers[0].id_field_next_question, false)
+                    }
+                ]
+            }
+        }else{
+            for (let i = 0; i < answers.length; i++) {
+                const element = answers[i];
+                new_children[i] = {
+                    name: element.name,
+                    attributes: element,
+                    children: (isAnAnswer)?await getChildren(element.id_next_question, false):await getChildren(id_next_question, true)
+                }
+            }
+        }
+    }else{
+        new_children = undefined
+    }
+    
+    return new_children
 }
 
+export async function getNewAnswers(tree_answers){
+    let new_children = {}
+    for (let i = 0; i < tree_answers.length; i++) {
+        if(tree_answers[i].id_next_question && tree_answers[i].id_next_question != 0){
+            const question = await db.query(`SELECT * FROM sr_cost_calculator_question WHERE id='${tree_answers[i].id_next_question}'`)
+            new_children.name = question[0].name
+            new_children.attributes = question[0]
+            let answers = undefined
+            if(new_children.attributes.answer_is_a_field == 1){
+                answers = [{answer_is_a_field: 1, id_next_question: new_children.id_field_next_question}]
+            }else{
+                answers = await db.query(`SELECT * FROM sr_cost_calculator_answer WHERE id_question='${new_children.id_next_question}' ORDER BY id ASC LIMIT 50`)
+            }
+            new_children.children = await getNewAnswers(answers)
+        }
+    }
+    return new_children
+}
